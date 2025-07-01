4c9e69fcc05132c25090c72498bbbeaa9869f197
federico-serron <f.serron.umpierrez@gmail.com>
2025-07-01 16:23:23 +0930

    Working on navbar and homepage

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,25 +3,25 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav class="navbar navbar-expand-lg navbar-light bg-light">
-			<div class="container-fluid">
-				<a class="navbar-brand" href="#">FedeTest</a>
-				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
+		<nav className="navbar navbar-expand-lg navbar-light bg-light">
+			<div className="container-fluid">
+				<a className="navbar-brand" href="#">FedeTest</a>
+				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
 				</button>
-				<div class="collapse navbar-collapse" id="navbarNav">
-					<ul class="navbar-nav ms-auto">
-						<li class="nav-item">
-							<a class="nav-link active" aria-current="page" href="#">Inicio</a>
+				<div className="collapse navbar-collapse" id="navbarNav">
+					<ul className="navbar-nav ms-auto">
+						<li className="nav-item">
+							<a className="nav-link active" aria-current="page" href="#">Inicio</a>
 						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">Login</a>
+						<li className="nav-item">
+							<a className="nav-link" href="#">Login</a>
 						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">Pricing</a>
+						<li className="nav-item">
+							<a className="nav-link" href="#">Pricing</a>
 						</li>
-						<li class="nav-item">
-							<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+						<li className="nav-item">
+							<a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
 						</li>
 					</ul>
 				</div>
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -15,9 +15,11 @@ import { Footer } from "./component/footer";
 const Layout = () => {
     //the basename is used when your project is published in a subdirectory and not in the root of the domain
     // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-    const basename = process.env.BASENAME || "";
+    // const basename = process.env.BASENAME || "";
+    const basename = "";
 
-    if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
+
+    if (!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL />;
 
     return (
         <div>
